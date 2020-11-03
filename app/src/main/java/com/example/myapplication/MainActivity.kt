e1ce5a790413c252b97716c098fc347e1240127b
YDJiang <jan4984@gmail.com>
2020-11-03 10:41:00 +0800

    load url while double tap

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.example.myapplication
 
 import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.EditText
+import android.widget.TextView
 
 
 class MainActivity : Activity() {
     lateinit var webview: WebView
+    lateinit var gestureDetector:GestureDetector
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,10 +35,37 @@ class MainActivity : Activity() {
             }
         }
         webview.loadUrl("http://m.xbiquge.la")
+
+        gestureDetector = GestureDetector(this, object : SimpleOnGestureListener() {
+            override fun onDoubleTap(e: MotionEvent): Boolean {
+                Log.d("m", "onDoubleTap")
+                var txt:TextView
+                var dialog = AlertDialog.Builder(this@MainActivity).apply {
+                    setView(EditText(this@MainActivity).apply {
+                        layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,200)
+                        setText(webview.url ?: "", TextView.BufferType.EDITABLE)
+                        txt=this
+                    })
+                    setNeutralButton("OK", DialogInterface.OnClickListener { _: DialogInterface, _: Int ->
+                        webview.loadUrl(txt.text.toString())
+                    })
+                }.create()
+                dialog.show()
+                txt.setSelectAllOnFocus(true)
+                txt.requestFocus()
+                return true
+            }
+        })
+
+        webview.setOnTouchListener { _: View, motionEvent: MotionEvent ->
+            gestureDetector.onTouchEvent(motionEvent)
+            false
+        }
     }
 
     override fun onBackPressed() {
         webview.goBack()
     }
+
 
 }
